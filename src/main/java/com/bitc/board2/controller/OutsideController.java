35fdb5c1a03983a54e1fd6ee94401c3327838247
lecture-bisper <bisper@outlook.kr>
2022-11-18 16:38:03 +0900

    외부 폴더 등록하여 사용하기.

---
+++ src/main/java/com/bitc/board2/controller/OutsideController.java
@@ -0,0 +1,16 @@
+package com.bitc.board2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.concurrent.ExecutionException;
+
+@Controller
+public class OutsideController {
+
+  @RequestMapping(value = "/outside/imgLoad", method = RequestMethod.GET)
+  public String imgLoad() throws Exception {
+    return "outside/imgLoad";
+  }
+}
